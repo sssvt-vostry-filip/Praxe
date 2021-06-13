04e16fa09ecb8f2db111b64bff3254fe15808f49
sssvt-vostry-filip <74659351+sssvt-vostry-filip@users.noreply.github.com>
2021-06-13 16:26:30 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href="Sass/style.css" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Úniková hra</title>
+
+
+
+</head>
+<body>
+    <header>
+        <div>
+            <h1></h1>
+        </div>
+    </header>
+    <main>
+        <section class="Intro">
+            <div class="Intro-Box">
+                <h3><a>Výtejte na stránce s únikovou hrou! Jedna z mála her, která je v hodná pro všechny věkové kategorie!</a></h3>
+            </div>
+            <div class="Intro-Box1">
+                <h3>Vyzkoušejte svoje myšlení a svou zvědavost a ponořte se do této unikové hry! V této hře si vás otestujeme jestli na to máte!</h3>
+            </div>
+        </section>
+        <section  class="Main">
+                <div>
+                    <div class="corners">
+                        <div class="top left"></div>
+                        <div class="top right"></div>
+                        <div class="bottom right"></div>
+                        <div class="bottom left"></div>
+                        <h1>
+                            Úniková Hra
+                        </h1>
+                    </div>
+
+                </div>
+
+        </section>
+        <section  class="Play">
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                        <button onclick="window.location.href='/utek.php'"><a>
+                            Hrát!</a>
+                        </button>
+                </div>
+
+        </section>
+    </main>
+
+
+<footer>
+    <div>
+
+    </div>
+</footer>
+</body>
+</html>
---
+++ utek.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Sass/styleutek.css">
+
+    <title>Útek</title>
+</head>
+<body>
+    <header>
+        <H1>Kapitola první: Útěk</H1>
+    </header>
+    <main>
+        <section class="text">
+                <a>Jste ve vězení jménem „Homins“. Jste v cele a vůbec nic si nepamatujete. Přijde najednou dozorčí a křičí „vstávej ty jedna líná kryso!“. Vy se leknete a zeptáte se ho proč jste tady.
+                    On vám poví „to nikdo neví na začátku, ale zachvíli to poznáš!“. Dotal jsi dopis, kde se píše „Promiň, ale tohle sis zasloužil, ale možná se ti to hodí „HDN + OIY“. Pavel“. Z ničeho nic spolubydlící na tebe jenom celou dobu kouká a asi nemá v plánu se s tebou bavit. Začíná večerka a ty by sis měl už jít lehnout. Ráno se probudíš a jdeš na snídani. Přijde za tebou jeden člověk, vypadá jako blázen a opakuje „ťiká to sakra, tikáto. Víš ?“
+                </a>
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
